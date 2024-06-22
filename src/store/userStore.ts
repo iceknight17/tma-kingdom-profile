0d052e27e7006ac5f835287acf926a74faaa2d68
iceknight17 <iceknight9717@gmail.com>
2024-06-21 23:46:39 -0500

    Update userStore.ts

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -19,17 +19,17 @@ type UserStore = {
 }
 
 export const useUserStore = create<UserStore>((set) => ({
-  // currentUser: null,
-  currentUser: {
-    id: 1,
-    username: 'testuser',
-    fullname: 'Test User',
-    user_chat_id: '7189498397',
-    wallet_address: 'qwesdf',
-    active_point: 0,
-    blue_stars: 1230,
-    red_stars: 5000,
-  },
+  currentUser: null,
+  // currentUser: {
+  //   id: 1,
+  //   username: 'testuser',
+  //   fullname: 'Test User',
+  //   user_chat_id: '7189498397',
+  //   wallet_address: 'qwesdf',
+  //   active_point: 0,
+  //   blue_stars: 1230,
+  //   red_stars: 5000,
+  // },
   // setCurrentUser: (user: GameUser) => set((state) => ({currentUser: user})),
   setCurrentUser: (user: GameUser) => set(() => ({currentUser: user})),
 }));
